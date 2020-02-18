7420e32b2b45700c776b9c5036e352b65296c7ea
MReusens <michael.reusens@kuleuven.be>
2020-02-18 14:17:50 +0100

    added insight in logistic model

---
+++ hlmos/src/collect_tweets.py
@@ -0,0 +1,91 @@
+import tweepy
+import pandas as pd
+import datetime as DT
+import time
+##
+## Connect to Twitter API
+##
+
+with open('../keys') as f:
+    keys = f.readlines()
+keys = [x.strip() for x in keys]
+consumer_key = keys[0]
+consumer_secret = keys[1]
+access_token  = keys[2]
+access_token_secret = keys[3]
+
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+
+
+##
+## Collect tweets function and save them
+##
+def collect_tweets(search, keyword, location=None, location_granularity = 'country',lang='nl', result_type='mixed', limit=0, retweets=False):
+    today = DT.date.today()
+    week_ago = today - DT.timedelta(days=7)
+    week_ago = week_ago.strftime("%Y-%m-%d")
+    date_since = week_ago
+
+    q = keyword
+
+
+
+    search_query = "{0}".format(keyword)
+    if location != None:
+        places = api.geo_search(query=location, granularity=location_granularity)
+        place_id = places[0].id
+        search_query = search_query+"&place:{1}".format(place_id)
+
+
+    if not retweets:
+        search_query = search_query + " -filter:retweets"
+    tweets = tweepy.Cursor(search, q=search_query, lang=lang, tweet_mode='extended',since=date_since)
+    tweets = tweets.items()
+    x = []
+    counter = 0
+    while True:
+        try:
+            tweet = tweets.next()
+            print(counter)
+            if counter > limit:
+                break
+            counter = counter + 1
+            x.append([tweet.id_str, keyword, tweet.full_text, tweet.created_at, tweet.lang, tweet.retweeted,
+                      tweet.author.screen_name, tweet.author.name, tweet.source])
+        except tweepy.TweepError:
+            print('sleeping')
+            time.sleep(60 * 15)
+            continue
+        except StopIteration:
+            break
+
+    x = pd.DataFrame.from_records(x, columns=["doc_id", "search_string", "text", "created_at", "language", "is_retweet",
+                                              "author", "author_name", "source"])
+    return (x)
+
+
+## A. data collection based on emoticons
+
+nb_tweets_to_collect = 5000
+
+positive_emoji = [":)", ":-)", ":D", ":-D", ": )"]
+positive_search_string = " OR ".join(positive_emoji)
+
+positive_search_string = positive_emoji[0]
+x = collect_tweets(api.search, keyword=positive_search_string, location=None, location_granularity = 'country', lang="nl", result_type="mixed", limit=nb_tweets_to_collect)
+x.to_csv("tweets_positive.csv", encoding='utf-8', index=False, sep=";")
+x.to_pickle("tweets_positive.pck", compression=None)
+
+
+negative_emoji = [":(", ": (", ":'("]
+negative_search_string = " OR ".join(negative_emoji)
+x = collect_tweets(api.search, keyword=negative_search_string, location=None, location_granularity = 'country', lang="nl", result_type="mixed", limit=nb_tweets_to_collect)
+x.to_csv("tweets_negative.csv", encoding='utf-8', index=False, sep=";")
+x.to_pickle("tweets_negative.pck", compression=None)
---
+++ hlmos/src/sentiment_model.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 22:48:37 2019
+
+@author: Marc Callens, Michael Reusens
+"""
+# eerst "pip install tweepy" uitvoeren in de command line van anaconda prompt
+# of eerst !pip install in IPhyton console
+import pandas as pd
+import datetime as DT
+
+
+
+tweets_file_location=  'C:/Users/Michael/Statistiek Vlaanderen/hlmos-statistiek-vlaanderen-twitter/hlmos/src/'
+#read in tweets
+negative = pd.read_csv(tweets_file_location+'tweets_negative.csv', sep = ";")
+positive = pd.read_csv(tweets_file_location+'tweets_positive.csv', sep = ";")
+
+
+print(len(positive))
+print(len(negative))
+
+#Ideally we put these in a file to be read in both be the tweet collection code and the sentiment modelling code to reduce chance of inconsistenties
+positive_emoji = [":)", ":-)", ":D", ":-D", ": )"]
+negative_emoji = [":(", ": (", ":'("]
+all_emoji = positive_emoji+negative_emoji
+
+#Uses Bert on tweets
+# https://towardsdatascience.com/russian-troll-tweets-classification-using-bert-abec09e43558
+
+
+
+
+
+# ## X. data collection based on emoticons
+#
+# x = collect_tweets(api.search, keyword="vlaanderen", lang="nl", result_type="mixed",
+#                    limit=100, retweets=False)
+# y = x[~x['text'].str.contains("RT @")]
+# y = x[~x['text'].str.startswith("RT @")]
+# x.to_csv("tweets_vla.csv", encoding='utf-8', index=False, sep=";")
+# x.to_pickle("tweets_vla.pck", compression=None)
+#
+# x = collect_tweets(api.search, keyword=":(", lang="nl", result_type="mixed", limit=5000)
+# x.to_csv("tweets_negative.csv", encoding='utf-8', index=False, sep=";")
+# x.to_pickle("tweets_negative.pck", compression=None)
+#
+# ## B. data collection based on general tweets
+#
+# ##### here ends data collection
+#
+# #################################
+# ##### Part II here starts processing
+# ##################################
+# positive = pd.read_pickle("tweets_positive.pck")
+# negative = pd.read_pickle("tweets_negative.pck")
+#
+db = positive.append(negative)
+db['target'] = db['search_string']
+db.target.value_counts()
+
+# db
+
+##
+## Make a penalised logistic regression model
+##
+import re, nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem.snowball import DutchStemmer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+nltk.download('punkt')
+
+stemmer = DutchStemmer(ignore_stopwords=False)
+
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+
+def tokenize(text):
+    text = re.sub("[^a-zA-Z]", " ", text)
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
+
+
+tokenize('We proberen even de stemmer uit')
+
+#TODO EXCLUDE MORE SMILEYS
+## Maak een document/term/matrix
+vectorizer = CountVectorizer(analyzer='word', tokenizer=tokenize, lowercase=True,
+                             stop_words=all_emoji, min_df=0.001)
+term_index_map = pd.DataFrame({'i' : list(vectorizer.vocabulary_.values()), 'term' : list(vectorizer.vocabulary_.keys())})
+term_index_map = term_index_map.sort_values(by='i')
+
+txt = db.text.tolist()
+dtm = vectorizer.fit_transform(txt)
+dtm_nd = dtm.toarray()
+dtm_nd.shape
+vocab = vectorizer.get_feature_names()
+
+## Maak een logistische regressie en zie naar de parameters van het model
+X_train, X_test, y_train, y_test = train_test_split(dtm_nd, db.target,
+                                                    train_size=0.75, random_state=1234)
+log_model = LogisticRegression(C=1, penalty="l1")
+log_model = log_model.fit(X=dtm_nd, y=db['target'])
+log_model.get_params()
+
+
+
+## Hoe goed werkte dit model op de test data
+y_pred = log_model.predict(X_test)
+print("Testing the testing dataset accuracy...")
+print(classification_report(y_test, y_pred))
+
+# optimizing parameter C
+# gridsearch voor C (=1)
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {'C': np.arange(0.01, 100, 0.25).tolist()}
+param_grid = {'C': [0.1, 0.5, 0.75, 0.8, 0.9, 1.1, 1.4, 5, 10, 20, 30]}
+
+log_model = LogisticRegression(penalty="l1", solver="liblinear")
+log_model_cv = GridSearchCV(log_model, param_grid, cv=5)
+
+# Fit it to the data
+log_model_cv.fit(X_train, y_train)
+
+# Print the tuned parameters and score
+print("Tuned Logistic Regression Parameters: {}".format(log_model_cv.best_params_))
+print("Best score is {}".format(log_model_cv.best_score_))
+
+log_model = LogisticRegression(C=1, penalty="l1", solver="liblinear")
+log_model = log_model.fit(X=X_train, y=y_train)
+log_model.get_params()
+log_model.coef_
+term_index_map['coef'] = log_model.coef_[0]
+#Inspect which words have highest coefficients
+print('top keywords negatief sentiment')
+term_index_map.sort_values(by='coef', ascending = True).head(20)
+print('top keywords positief sentiment')
+term_index_map.sort_values(by='coef', ascending = False).head(20)
+
+
+## Hoe goed werkte dit model op de test data
+y_pred = log_model.predict(X_test)
+print("Testing the testing dataset accuracy...")
+print(classification_report(y_test, y_pred))
+
+y_pred
+
+y_test
+
+vectorizer = CountVectorizer(analyzer='word', tokenizer=tokenize, lowercase=True,
+                             stop_words=all_emoji, vocabulary=vocab)
+txt = ["een nieuw tweet", "ja zuper"]
+dtm = vectorizer.fit_transform(txt)
+log_model.predict(dtm)
+
+vocab
